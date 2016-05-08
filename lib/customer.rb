86f4074c7a0813829f5e1e17c7977ee2554f1f6b
Artur Jonczyk <artur.jonczyk@gmail.com>
2016-05-08 21:22:45 +0200

    Added possibility to display all transaction by customer

--- lib/customer.rb
+++ lib/customer.rb
@@ -30,4 +30,15 @@ class Customer
             raise OutOfStockError, "#{product.title} is out of stock."
         end
     end
+
+    # returning all transactions done by customer
+    def all_transactions
+        Transaction.all.select { |t| t.customer.name == @name}
+    end
+
+    def display_all_transactions
+        all_transactions.each do |t|
+            puts "Transaction ID: #{t.id} - #{t.product.title}"
+        end
+    end
 end
